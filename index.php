2a1321e33af9172421f7bec8011e104785b8640a
andrebayup14 <abayup1498@gmail.com>
2023-06-26 13:07:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Input Data Makanan</title>
 </head>
 <body>
     <h1>Isikan Data Makanan yang Akan Ditampilkan</h1>
