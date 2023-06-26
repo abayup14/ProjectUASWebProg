49c4940a331f1483b7aba63080a61ef53f854a26
andrebayup14 <abayup1498@gmail.com>
2023-06-26 13:28:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php 
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,23 +10,45 @@
 </head>
 <body>
     <h1>Isikan Data Makanan yang Akan Ditampilkan</h1>
-    <form action="index.php" method="POST">
+    <form action="order.php" method="POST" enctype="multipart/form-data">
         <p>
             <label>Kode Makanan : </label>
-            <input type="text" name="" id="txtkode">
+            <input type="text" name="txtkode" id="txtkode">
         </p>
         <p>
             <label>Nama Makanan : </label>
-            <input type="text" name="" id="txtnama">
+            <input type="text" name="txtnama" id="txtnama">
         </p>
         <p>
             <label>Harga Makanan : </label>
-            <input type="number" name="" id="txtharga">
+            <input type="number" name="txtharga" id="txtharga">
         </p>
         <p>
             <label>Alamat Foto Makanan : </label>
-            <input type="url" name="" id="txturl">
+            <input type="url" name="txturl" id="txturl">
         </p>
-    </form> 
+        <p>
+            <input type="button" value="Masukkan" name="btnmasuk">
+        </p>
+    </form>
+    <?php 
+        if (isset($_SESSION["makanan"])) {
+            $arr_makanan = $_SESSION["makanan"];
+        }
+
+        if (isset($_POST["btnmasuk"])) {
+            $makanan = array(
+                "kode" => $_POST["txtkode"],
+                "nama" => $_POST["txtnama"],
+                "harga" => $_POST["txtharga"],
+                "url" => $_POST["txturl"]
+            );
+
+            $arr_makanan = $makanan;
+            $_SESSION["makanan"] = $arr_makanan;
+        }
+
+        header("Location:index.php");
+    ?>
 </body>
 </html>
