00ea0144acb4476472b4dd5ae4863848ddc86c59
andrebayup14 <abayup1498@gmail.com>
2023-06-26 16:48:40 +0700

    Update Senin

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
-<?php 
-    session_start();
+<?php
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Input Data Makanan</title>
 </head>
+
 <body>
     <h1>Isikan Data Makanan yang Akan Ditampilkan</h1>
-    <form action="order.php" method="POST" enctype="multipart/form-data">
+    <form action="index.php" method="POST" enctype="multipart/form-data">
         <p>
             <label>Kode Makanan : </label>
             <input type="text" name="txtkode">
@@ -31,28 +33,25 @@
             <input type="submit" value="Masukkan" name="btnmasuk">
         </p>
         <p>
-            <input type="submit" value="Menuju halaman Order" name="btnOrder">
+            <input type="submit" value="Menuju halaman Pesan" name="btnOrder">
         </p>
     </form>
 </body>
 <?php
-    /*if (isset($_SESSION["makanan"])) {
-        $arr_makanan = $_SESSION["makanan"];
-    }
+if (isset($_POST["btnmasuk"])) {
+    $makanan = array(
+        "kode" => $_POST["txtkode"],
+        "nama" => $_POST["txtnama"],
+        "harga" => $_POST["txtharga"],
+        "url" => $_POST["txturl"]
+    );
 
-    if (isset($_POST["btnmasuk"])) {
-        $makanan = array(
-            "kode" => $_POST["txtkode"],
-            "nama" => $_POST["txtnama"],
-            "harga" => $_POST["txtharga"],
-            "url" => $_POST["txturl"]
-        );
-        
-        $_SESSION["makanan".count($_SESSION)] = $makanan;
-    }*/
+    $_SESSION["makanan" . count($_SESSION)] = $makanan;
+}
 
-    if (isset($_POST["btnOrder"])) {
-        header("location: order.php");
-    }
+if (isset($_POST["btnOrder"])) {
+    header("location: order.php");
+}
 ?>
+
 </html>
--- order.php
+++ order.php
@@ -1,138 +1,89 @@
 <?php
 session_start();
 
-if (isset($_SESSION["makanan"])) {
-        $arr_makanan = $_SESSION["makanan"];
-    }
-
-    if (isset($_POST["btnmasuk"])) {
-        $makanan = array(
-            "kode" => $_POST["txtkode"],
-            "nama" => $_POST["txtnama"],
-            "harga" => $_POST["txtharga"],
-            "url" => $_POST["txturl"]
-        );
-        
-        $_SESSION["makanan".count($_SESSION)] = $makanan;
-
-        header("location:index.php");
-    }
-
+if (isset($_SESSION)) {
+    $arr_makanan = $_SESSION;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Order Makanan</title>
     <style type="text/css">
-    .katalog
-    {
-        width:"40%"; float:left;
-        border-style: solid;
-        margin: 5px;
-        margin-left: 25px;
-        margin-right: 25px;
-        margin-bottom: 25px;
-        margin-top: 25px;
-    }
-    img
-    {
-        width: 200px;
-        height: 140px;
-        text-align:center;
-        margin: 5px;
-    }
-    h2{
-        margin-top: 0px;
-        margin-left: 5px;
-        margin-right: 5px;
-        margin-bottom: 5px;
-    }
-    h3{
-        margin: 5px;
-    }
-    button{
-        margin: 6px;
-    }
+        .katalog {
+            width: "40%";
+            float: left;
+            border-style: solid;
+            margin: 5px;
+            margin-left: 25px;
+            margin-right: 25px;
+            margin-bottom: 25px;
+            margin-top: 25px;
+        }
 
-    .menu{
-        width:70%;
-        background: pink;
-        float: left;
-        margin-right: 50px;
-    }
+        img {
+            width: 200px;
+            height: 140px;
+            text-align: center;
+            margin: 5px;
+        }
 
-    .pilihan{
-        float: right;
-        width: 250px;
-        background: yellow;
-    }
+        h2 {
+            margin-top: 0px;
+            margin-left: 5px;
+            margin-right: 5px;
+            margin-bottom: 5px;
+        }
 
-    #pilihanku{
-        margin: 5px;
-    }
+        h3 {
+            margin: 5px;
+        }
+
+        button {
+            margin: 6px;
+        }
+
+        .menu {
+            width: 70%;
+            background: pink;
+            float: left;
+            margin-right: 50px;
+        }
+
+        .pilihan {
+            float: right;
+            width: 250px;
+            background: yellow;
+        }
+
+        #pilihanku {
+            margin: 5px;
+        }
     </style>
 </head>
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.7.0.js"></script>
+
 <body>
     <h1>Today Menu: </h1>
     <div class="menu">
-        <?php 
-        foreach($arr_makanan as $makanan){
+        <?php
+        foreach ($arr_makanan as $makanan) {
             echo "<div class='katalog'>";
-            echo '<img src='.$makanan['url'].'>';
-            echo '<h2>'.$makanan['nama'].'</h2>';
-            echo '<h3> Rp'.$makanan['harga'].'</h3>';
-            echo '<button id="btnPilih'.$makanan['kode'].'">Pilih</button>';
+            echo "<img src='" . $makanan['url'] . "'>";
+            echo '<h2>' . $makanan['nama'] . '</h2>';
+            echo '<h3> Rp' . $makanan['harga'] . '</h3>';
+            echo "<button id='btnPilih" . $makanan['kode'] . "'>Pilih</button>";
             echo "</div>";
         }
         ?>
-
-        <!-- <div class="katalog">
-            <img src="images/rendang.jpg">
-            <h2>Rendang</h2>
-            <h3>Rp. 34.000</h3>
-            <button id="btnPilih">Pilih</button>
-        </div>
-
-        <div class="katalog">
-            <img src="images/rendang.jpg">
-            <h2>Rendang</h2>
-            <h3>Rp. 34.000</h3>
-            <button id="btnPilih">Pilih</button>
-        </div>
-
-        <div class="katalog">
-            <img src="images/rendang.jpg">
-            <h2>Rendang</h2>
-            <h3>Rp. 34.000</h3>
-            <button id="btnPilih">Pilih</button>
-        </div>
-
-        <div class="katalog">
-            <img src="images/rendang.jpg">
-            <h2>Rendang</h2>
-            <h3>Rp. 34.000</h3>
-            <button id="btnPilih">Pilih</button>
-        </div>
-
-        <div class="katalog">
-            <img src="images/rendang.jpg">
-            <h2>Rendang</h2>
-            <h3>Rp. 34.000</h3>
-            <button id="btnPilih">Pilih</button>
-        </div>
-        <div class="katalog">
-            <img src="images/rendang.jpg">
-            <h2>Rendang</h2>
-            <h3>Rp. 34.000</h3>
-            <button id="btnPilih">Pilih</button>
-        </div> -->
     </div>
 
     <div class="pilihan">
         <h1 id="pilihanku">Pilihanku: </h1>
     </div>
 </body>
+
 </html>
