04ff9ac41e57717377e79ce1f7890975d525b1f9
s160421001 <stchrist03@gmail.com>
2023-06-26 13:27:16 +0700

    order.php

--- order.php
+++ order.php
@@ -1,10 +1,22 @@
+<?php
+session_start();
+
+if (isset($_SESSION["order"])) {
+    $arr_std = $_SESSION["order"];
+}
+else{
+    header("location: index.php");
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Order Makanan</title>
 </head>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.7.0.js"></script>
 <body>
     
 </body>
