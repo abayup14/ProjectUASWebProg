a9f4e992fb6dac4ebebb8346e4714c2a7df96b78
andrebayup14 <abayup1498@gmail.com>
2023-06-26 13:01:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,24 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Isikan Data Makanan yang Akan Ditampilkan</h1>
+    <form action="index.php" method="POST">
+        <p>
+            <label for="">Kode Makanan : </label>
+            <input type="text" name="" id="txtkode">
+        </p>
+        <p>
+            <label for="">Nama Makanan : </label>
+            <input type="text" name="" id="txtnama">
+        </p>
+        <p>
+            <label for="">Harga Makanan : </label>
+            <input type="number" name="" id="txtharga">
+        </p>
+        <p>
+            <label for="">Alamat Foto Makanan : </label>
+            <input type="url" name="" id="txturl">
+        </p>
+    </form> 
 </body>
 </html>
